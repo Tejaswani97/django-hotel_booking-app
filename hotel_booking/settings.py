bd03020fd8e00118953d28cb6c2b3e48fdf68a89
Tejaswani97 <tejaswani.chinni7@gmail.com>
2026-05-22 14:06:01 +0530

    Prepared project for deployment

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hhjz$etj7qr$8xtihgus9t&8_(#4sdd05bd0cdj1i)q)8#ac9a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,4 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
